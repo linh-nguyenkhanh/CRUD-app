72c44bdc26c3a62abe49dd43f059db347377266a
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-02-11 18:13:42 +0200

    created CRUD functions in server

--- server/index.js
+++ server/index.js
@@ -10,11 +10,40 @@ app.use(express.json()); //req.body: access to json data
 // routes
 
 // create a todo
+app.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *",
+      [description]
+    );
 
+    res.json(newTodo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 // get all todo
 
-// get a todo
+app.get("/todos", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo");
 
+    res.json(allTodos.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+// get a todo
+app.get("/todos", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo");
+    res.json(allTodos.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 // update a todo
 
 // delete a todo
