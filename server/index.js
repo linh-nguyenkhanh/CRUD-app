a0de016d4952db878be3c60fcc9d571bcb64d67d
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-02-11 18:34:55 +0200

    built resful api

--- server/index.js
+++ server/index.js
@@ -35,18 +35,49 @@ app.get("/todos", async (req, res) => {
   }
 });
 
-// get a todo
-app.get("/todos", async (req, res) => {
+// get a todo,req.params is to specified id
+app.get("/todos/:id", async (req, res) => {
   try {
-    const allTodos = await pool.query("SELECT * FROM todo");
-    res.json(allTodos.rows);
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+
+    res.json(todo.rows[0]);
+    // console.log(req.params);
   } catch (err) {
     console.error(err.message);
   }
 });
 // update a todo
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateTodo = await pool.query(
+      "UPDATE  todo SET description = $1 WHERE todo_id = $2",
+      [description, id]
+    );
+
+    res.json("todo was updated");
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 // delete a todo
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+
+    res.json("todo was deleted ");
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
